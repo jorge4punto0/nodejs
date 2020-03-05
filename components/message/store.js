21a51263b5cc57f29eb3c420c4a1b30c1834a4de
Jorgelisapa <jorgelisapa@gmail.com>
2020-03-05 16:41:13 -0500

    Relacionando nuestras entidades: usuarios y mensajes - v26

--- components/message/store.js
+++ components/message/store.js
@@ -7,14 +7,24 @@ function addMessage(message) {
 }
 
 async function getMessages(filterUser) {
-    let filter = {};
-    if (filterUser !== null) {
-        filter = {
-            user: filterUser
-        };
-    }
-    const messages = await Model.find(filter);
-    return messages
+    return new Promise((resolve, reject) => {
+        let filter = {};
+        if (filterUser !== null) {
+            filter = {
+                user: filterUser
+            };
+        }
+        Model.find(filter)
+            .populate('user')
+            .exec((error, populated) => {
+                if(error) {
+                    reject(error);
+                    return false;
+                }
+                resolve(populated);
+            })
+    });
+
 }
 
 function removeMessage(id) {
